639d02de68253ac1508bda3b22891563d5d6009e
Ridam-Kukde <ridamkukde01@gmail.com>
2024-08-03 00:18:27 +0530

    feat: reset button and stable navbar

--- src/components/Layout1/Map.js
+++ src/components/Layout1/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
@@ -7,27 +7,47 @@ import L from 'leaflet';
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
 });
-
+const ResetButton = ({ resetView }) => {
+    return (
+        <button
+            className="bg-blue-500 text-white py-2 px-4 rounded-full absolute top-4 right-4 shadow-md"
+            onClick={resetView}
+        >
+            Reset View
+        </button>
+    );
+};
 const Map = () => {
-  return (
-    <div style={{ marginTop: '2rem' }}>
-      <MapContainer center={[23.1765, 75.7885]} zoom={10} style={{ height: '400px', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={[23.1765, 75.7885]}>
-          <Popup>
-            My Current Location: Ujjain, MP, India
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+    const initialPosition = [23.1765, 75.7885];
+
+    const mapRef = useRef();
+
+    const resetView = () => {
+        const map = mapRef.current;
+        if (map) {
+            map.setView(initialPosition, 12);
+        }
+    };
+    return (
+        <div className="mt-8 mb-10 px-10 flex justify-center">
+            <MapContainer className="h-96 w-full" center={[23.1765, 75.7885]} zoom={10} style={{ height: '400px', width: '100%' }}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+
+                />
+                <Marker position={[23.1765, 75.7885]}>
+                    <Popup>
+                        My Current Location: Ujjain, MP, India
+                    </Popup>
+                </Marker>
+                <ResetButton resetView={resetView} />
+            </MapContainer>
+        </div>
+    );
 };
 
 export default Map;
--- src/components/Layout1/NavBar.js
+++ src/components/Layout1/NavBar.js
@@ -1,32 +1,70 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Avatar, Menu, MenuItem, Box } from '@mui/material';
 import { Link } from 'react-scroll';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 export const NavBar = ({ darkMode, handleThemeChange }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = 'primary-search-account-menu';
+
   return (
-    <AppBar position="sticky" color="primary">
-      <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          My Portfolio
-        </Typography>
-        <Button color="inherit">
-          <Link to="about" smooth={true} offset={-70} duration={500}>About</Link>
-        </Button>
-        <Button color="inherit">
-          <Link to="skills" smooth={true} offset={-70} duration={500}>Skills</Link>
-        </Button>
-        <Button color="inherit">
-          <Link to="projects" smooth={true} offset={-70} duration={500}>Projects</Link>
-        </Button>
-        <Button color="inherit">
-          <Link to="contact" smooth={true} offset={-70} duration={500}>Contact</Link>
-        </Button>
-        <IconButton color="inherit" onClick={handleThemeChange}>
-          {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+    <Box>
+      <AppBar position="sticky" color="primary">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            My Portfolio
+          </Typography>
+          <Button color="inherit">
+            <Link to="about" smooth={true} offset={-70} duration={500}>About</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="skills" smooth={true} offset={-70} duration={500}>Skills</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="projects" smooth={true} offset={-70} duration={500}>Projects</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="contact" smooth={true} offset={-70} duration={500}>Contact</Link>
+          </Button>
+          <IconButton color="inherit" onClick={handleThemeChange}>
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box position="fixed" top="1rem" right="1rem">
+        <IconButton
+          edge="end"
+          aria-label="account of current user"
+          aria-controls={menuId}
+          aria-haspopup="true"
+          onClick={handleProfileMenuOpen}
+          color="inherit"
+        >
+          <Avatar alt="Profile Picture" src="path-to-your-profile-picture.jpg" />
         </IconButton>
-      </Toolbar>
-    </AppBar>
+        <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          id={menuId}
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem onClick={handleMenuClose}>Settings</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Change Layout</MenuItem>
+        </Menu>
+      </Box>
+    </Box>
   );
 };
